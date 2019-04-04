02ae2fa1b38afcee1021967962bec70bc94b85e1
Castrucci <maximiliano.castrucci@igt.com>
2019-04-04 00:39:47 -0300

    fixed footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,47 +1,14 @@
 import React, { Component } from 'react';
 import './Footer.css'
-import fbLogo from './_ionicons_svg_logo-facebook.svg';
-import wpLogo from './_ionicons_svg_logo-whatsapp.svg';
-import inLogo from './_ionicons_svg_logo-instagram.svg';
 
 class Footer extends Component {
     render(){
         return(
             <footer className="footer">
-                <div className="footer__top">
-                    <div className="footer__top__social">
-                        <p>Seguime en las redes y mantenete al tanto de novedades ;)</p>
-                        <div className="footer_top__social_icons">
-                            <div className="footer__top__social__facebook">
-                                <a href="www."><img src={fbLogo} alt="Facebook logo" height="35" width="35"></img></a>
-                            </div>
-                            <div className="footer__top__social__whatsapp">
-                             <a href="www."><img src={wpLogo} alt="Whatsapp logo" height="35" width="35"></img></a>
-                            </div>
-                            <div className="footer__top__social__instagram">
-                                <a href="www."><img src={inLogo} alt="Instagram logo" height="35" width="35"></img></a>
-                            </div>
-                        </div>
+                    <div><h2>React</h2></div>
+                    <div className="footer__botoom_author">
+                        <h2>Diseño y Desarrollo por Maximiliano Castrucci</h2>
                     </div>
-                    <div className="footer__top__about"> 
-                        <p>
-                        Copyright (c) 2003 Reiki Usui Terapia
-                        All Rights Reserved
- 
-                        This product is protected by copyright and distributed under
-                        licenses restricting copying, distribution, and decompilation.
-                        </p>
-                    </div>
-                    <div className="footer__top__contact"> 
-                        <div>
-                            <p>Contactame</p>
-                        </div>
-
-                    </div>
-                </div>
-                <div className="footer__bottom">
-                    <div className="footer__botoom_author">Diseño: Maximiliano Castrucci</div>
-                </div>
             </footer>
         );
     }
