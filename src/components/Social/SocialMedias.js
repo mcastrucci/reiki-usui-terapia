794574072bcb9368d1d00798374cc7fd81336002
Castrucci <maximiliano.castrucci@igt.com>
2019-04-08 20:34:22 -0300

    tinified images

--- src/components/Social/SocialMedias.js
+++ src/components/Social/SocialMedias.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import './SocialMedias.css'
-import fbLogo from './_ionicons_svg_logo-facebook.svg';
-import wpLogo from './_ionicons_svg_logo-whatsapp.svg';
-import inLogo from './_ionicons_svg_logo-instagram.svg';
 
 class SocialMedias extends Component {
     render(){
         return(
             <div className="social-medias">
                 <div className="social-medias__facebook social-media_button">
-                    <a href="https://www.facebook.com/samanta.castrucci"><img src={fbLogo} alt="Facebook logo"></img></a>
+                    <a href="https://www.facebook.com/samanta.castrucci" className="fa fa-facebook"> </a>
                 </div>
-                <div className="social-medias__whatsapp social-media_button">
-                <img src={wpLogo} onClick={ ()=>this.props.onRouteChange('contact')} alt="Whatsapp logo"></img>
+                <div className="social-medias__whatsapp social-media_button fa fa-whatsapp" onClick={ ()=>this.props.onRouteChange('contact')}>
                 </div>
                 <div className="social-medias__Instagram social-media_button">
-                <a href="https://www.instagram.com/reikiusuiterapias/?hl=en"><img src={inLogo} alt="Instagram logo"></img></a>
+                <a href="https://www.instagram.com/reikiusuiterapias/?hl=en" className="fa fa-instagram"> </a>
                 </div>
             </div>
         );
